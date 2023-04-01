2d5b2a8a0ec287727dd2ed197b317f895922e4ee
edem-cyber <edem.agbakpe@gmail.com>
2023-04-01 05:53:18 +0000

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-green-600">Welcome to Todo App</div>
+    </div>
+  );
+}
+
+export default App;
